cfd55509840476b6be94d2884d3b48f34bbb50e5
Diego Prada-Gracia <prada.gracia@gmail.com>
2025-11-03 22:51:09 -0600

    Gas constant value fixed

--- pyunitwizard/constants/constants.py
+++ pyunitwizard/constants/constants.py
@@ -3,7 +3,7 @@ from typing import Any, Dict, Optional, Union, Tuple
 
 _constants = {
         'Avogadro': [6.02214076e+23, '1/mole'],
-        'Universal gas': [8.13446261815324, 'J/(kelvin*mole)'], # Avogadro * Boltzmann
+        'Universal gas': [8.31446261815324, 'J/(kelvin*mole)'], # Avogadro * Boltzmann
         'Boltzmann': [1.380649e-23, 'J/kelvin'],
         }
 
