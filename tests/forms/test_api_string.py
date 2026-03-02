e55001f06c03782775d98821fe153c96b03c9060
Diego Prada <prada.gracia@gmail.com>
2026-03-02 15:25:35 -0600

    ci(release): fix test import path and docs setuptools compatibility

--- tests/forms/test_api_string.py
+++ tests/forms/test_api_string.py
@@ -1,8 +1,34 @@
 import pytest
+from contextlib import contextmanager
 
 import pyunitwizard as puw
 from pyunitwizard._private.exceptions import LibraryWithoutParserError
-from tests.helpers import loaded_libraries
+
+
+@contextmanager
+def loaded_libraries(libraries):
+    previous_libraries = list(puw.configure.get_libraries_loaded())
+    previous_default_form = puw.configure.get_default_form()
+    previous_default_parser = puw.configure.get_default_parser()
+
+    puw.configure.reset()
+    puw.configure.load_library(libraries)
+    try:
+        yield
+    finally:
+        puw.configure.reset()
+        if previous_libraries:
+            puw.configure.load_library(previous_libraries)
+        else:
+            for library in ['pint', 'openmm.unit', 'unyt']:
+                try:
+                    puw.configure.load_library(library)
+                except (ImportError, ModuleNotFoundError):
+                    continue
+        if previous_default_form is not None:
+            puw.configure.set_default_form(previous_default_form)
+        if previous_default_parser is not None:
+            puw.configure.set_default_parser(previous_default_parser)
 
 
 def test_api_string_basic_ops_with_pint_defaults():
