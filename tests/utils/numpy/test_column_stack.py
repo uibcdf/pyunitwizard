5bb2f1bf909cdd7c384ee7379d5d300f60922608
Diego Prada <prada.gracia@gmail.com>
2026-03-02 15:07:53 -0600

    test(utils): complete stack and column_stack branch coverage

--- tests/utils/numpy/test_column_stack.py
+++ tests/utils/numpy/test_column_stack.py
@@ -16,3 +16,26 @@ def test_column_stack_1():
     assert value.shape==(10, 2)
 
 
+def test_column_stack_with_explicit_unit_and_tuple_output():
+    puw.configure.reset()
+    puw.configure.load_library(['pint'])
+
+    aa = puw.quantity([0, 1, 2], 'nm')
+    bb = puw.quantity([3, 4, 5], 'nm')
+    quantity = puw.utils.numpy.column_stack([aa, bb], to_unit='angstrom', value_type='tuple')
+    value = puw.get_value(quantity)
+    assert puw.get_unit(quantity) == puw.unit('angstrom')
+    np.testing.assert_allclose(value, [[0.0, 30.0], [10.0, 40.0], [20.0, 50.0]])
+
+
+def test_column_stack_list_output_and_invalid_value_type():
+    puw.configure.reset()
+    puw.configure.load_library(['pint'])
+
+    aa = puw.quantity([0, 1, 2], 'nm')
+    bb = puw.quantity([3, 4, 5], 'nm')
+    quantity = puw.utils.numpy.column_stack([aa, bb], value_type='list')
+    np.testing.assert_array_equal(puw.get_value(quantity), [[0, 3], [1, 4], [2, 5]])
+
+    with pytest.raises(ValueError):
+        puw.utils.numpy.column_stack([aa, bb], value_type='invalid')
--- tests/utils/numpy/test_stack.py
+++ tests/utils/numpy/test_stack.py
@@ -25,3 +25,24 @@ def test_stack_2():
     assert value.shape==(10, 2)
 
 
+def test_stack_with_explicit_unit_and_tuple_output():
+    puw.configure.reset()
+    puw.configure.load_library(['pint'])
+
+    list_list_quantities = [[puw.quantity(ii, 'nm') for ii in range(3)] for jj in range(2)]
+    quantity = puw.utils.numpy.stack(list_list_quantities, to_unit='angstrom', value_type='tuple')
+    value = puw.get_value(quantity)
+    assert puw.get_unit(quantity) == puw.unit('angstrom')
+    np.testing.assert_allclose(value, [[0.0, 10.0, 20.0], [0.0, 10.0, 20.0]])
+
+
+def test_stack_list_output_and_invalid_value_type():
+    puw.configure.reset()
+    puw.configure.load_library(['pint'])
+
+    list_list_quantities = [[puw.quantity(ii, 'nm') for ii in range(3)] for jj in range(2)]
+    quantity = puw.utils.numpy.stack(list_list_quantities, value_type='list')
+    np.testing.assert_array_equal(puw.get_value(quantity), [[0, 1, 2], [0, 1, 2]])
+
+    with pytest.raises(ValueError):
+        puw.utils.numpy.stack(list_list_quantities, value_type='invalid')
