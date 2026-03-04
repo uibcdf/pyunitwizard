ba98f47928a23775b90c78435f33a93d54c96f5f
Diego Prada <prada.gracia@gmail.com>
2026-03-04 06:25:43 +0000

    From refs/tags/0.21.1 7816587203e32b2b489a82488d54974ad11e6e64

--- pyunitwizard/_private/smonitor/meta.py
+++
@@ -1,3 +0,0 @@
-DOC_URL = "https://uibcdf.org/pyunitwizard"
-ISSUES_URL = "https://github.com/uibcdf/pyunitwizard/issues"
-API_URL = "https://uibcdf.org/pyunitwizard"
--- pyunitwizard/utils/sequences/is_quantity_value_sequence.py
+++
@@ -1,7 +0,0 @@
-from pyunitwizard.api import get_value, is_quantity
-from .is_sequence import is_sequence
-
-def is_quantity_value_sequence(item):
-    """Check if an item is a quantity whose value is a sequence."""
-    return is_quantity(item) and is_sequence(get_value(item))
-
